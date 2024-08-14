9377625d208aa4e4f28dfdd2bba69dd34044679b
kimyoonjae77 <kimyoonjae77@gmail.com>
2024-08-14 17:07:38 +0900

    124142
    124124124412

--- faq.js
+++ faq.js
@@ -14,4 +14,30 @@ $(document).ready(function () {
             $(this).addClass("active").next().stop().slideDown();
         }
     });
+
+    $(".language>p").click(function () {
+        // $(".language_box").stop().fadeOut();
+        $(".language_box").stop().fadeToggle();
+    });
+    $(window).click(function (e) {
+        const isFamMenu = Boolean(e.target.closest(".not_js"));
+        if (!isFamMenu) {
+            $(".language_box").stop().fadeOut(300);
+        }
+    });
+
+    $("#top_btn").click(function () {
+        $("html,body").stop().animate({ scrollTop: "0" }, 500);
+    });
+
+    $(".site_btn").click(function () {
+        $(".click_site").fadeToggle();
+        // $(".fa-angle-down").css({ transform: "rotate(180deg)" });
+    });
+    $(window).click(function (e) {
+        const isFamMenu2 = Boolean(e.target.closest(".site_btn"));
+        if (!isFamMenu2) {
+            $(".click_site").stop().fadeOut(300);
+        }
+    });
 });
